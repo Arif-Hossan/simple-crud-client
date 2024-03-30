d568c3c697f6fb03e9c9dea30f9dd5fb7e3f8388
Arif Hossan <arif.hossan.eu@gmail.com>
2024-03-31 03:23:13 +0600

    update data

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,28 +1,35 @@
 // import React from 'react';
 
-import { useLoaderData } from "react-router-dom";
-const handleDelate =_id =>{
-console.log('delete', _id);
-fetch( `http://localhost:5000/users/${_id}`,{
-    method:"Delete"
-})
-.then(res => res.json())
-.then(data => {
-    console.log(data);
-    if(data.deletedCount > 0){
-        alert('deleted success')
-    }
-})
-}
+import { useState } from "react";
+import { Link, useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const users= useLoaderData();
+    const loadedUsers= useLoaderData();
+    const [users,setUsers]= useState(loadedUsers);
+    const handleDelate =_id =>{
+        console.log('delete', _id);
+        fetch( `http://localhost:5000/users/${_id}`,{
+            method:"Delete"
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            if(data.deletedCount > 0){
+                alert('deleted')
+                const remaining = users.filter(user=>user._id !== _id)
+                setUsers(remaining);
+            }
+        })
+        }
     return (
         <div>
           <h2>  {users.length}</h2>
           {
             users.map(user => <p key={user._id}>
                 {user.name} : {user.email}
+                <Link to={`/update/${user._id}`}>
+                <button>Update</button>
+                </Link>
                 <button onClick={()=>handleDelate(user._id)}>X</button></p>)
           }
         </div>
